fea6206e1b3c68fc84d83acd83b2ccde33b0da13
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-10 22:25:57 -0600

    Update drink.js and ejs
    - Changed some styles
    - added html and ejs to show drink names

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const express = require('express')
 const PORT = process.env.PORT
 const app = express()
 const methodOverride = require('method-override')
+const drinks = require('./models/drinks')
 
-app.get('/', (req,res) => {
-    console.log("hello")
+// HOME ROUTE - Redirects to the localhost:3000/drinks
+app.get('/', (req,res) => res.redirect("/drinks"))
+
+// Displays the Drinks
+app.get('/drinks', (req,res) => {
+    res.render('drinks_index.ejs',
+    {
+        allDrinks:drinks
+    })
 })
+
 
 
 

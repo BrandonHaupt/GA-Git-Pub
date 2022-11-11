4919641d7ca04574144bfc0b550b9cec9beaea67
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-10 23:10:29 -0600

    create foods, food_show_foods
    - food is starting look like it's spelt funny
    - update a Show Route for foods
    - Capitalized first letter in food

--- server.js
+++ server.js
@@ -4,19 +4,22 @@ const PORT = process.env.PORT
 const app = express()
 // const methodOverride = require('method-override')
 const drinks = require('./models/drinks')
+const foods = require('./models/foods')
 
 // HOME ROUTE - Redirects to the localhost:3000/drinks
 app.get('/', (req,res) => res.redirect("/drinks"))
+
 
 // Displays the Drinks
 app.get('/drinks', (req,res) => {
     res.render('drinks_index.ejs',
     {
-        allDrinks:drinks
+        allDrinks:drinks,
+        allFoods:foods
     })
 })
 
-// SHOW Route
+// SHOW ROUTE for Drinks
 app.get("/drinks/:id", (req, res) => {
 
     res.render("drinks_show.ejs", {
@@ -25,6 +28,13 @@ app.get("/drinks/:id", (req, res) => {
     })
 })
 
+// SHOW ROUTE for Foods
+app.get("/foods/:id", (req, res) => {
+    res.render("food_show.ejs", {
+        food: foods[req.params.id],
+        id: req.params.id
+    })
+})
 
 
 // Server Listener
